52343313049801019966154aeed33f873791d43f
Hunter H <hunter.hester15@gmail.com>
2024-12-05 09:40:49 -0600

    adds day 3 partone

--- Day 3/AoC3.js
+++ Day 3/AoC3.js
@@ -2,5 +2,24 @@
 
 const file = Bun.file('input.txt');
 const data = await file.text();
+// const data = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
 
-console.log(data)
+var partOne = function(data) {
+    const pattern = /mul\(\s*(\d+)\s*,\s*(\d+)\s*\)/g;
+    let res = 0;
+
+    const matches = [...data.matchAll(pattern)];
+    matches.forEach((match, index) => {
+        console.log(`Match ${index + 1}: ${match[0]}, Numbers: ${match[1]}, ${match[2]}`);
+    });
+
+    matches.forEach(match => {
+        const num1 = parseInt(match[1]);
+        const num2 = parseInt(match[2]);
+        res += num1 * num2;
+    });
+
+    console.log(res);
+};
+
+partOne(data);
